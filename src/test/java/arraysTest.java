789d39ef9f5c8da54f1f4311d61fe6e5e7ac1a16
AlexKysil <38106741+AlexKysil@users.noreply.github.com>
2021-07-29 14:55:21 +0300

    array list reminder list reminder

--- src/test/java/arraysTest.java
+++ src/test/java/arraysTest.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class arraysTest {
     public static void main(String[] args){
         String[] array1 = new String [4];
@@ -15,6 +19,26 @@ public class arraysTest {
         String[] array2 = {"Java", "Python", "C#", "JavaScript"};
         for(String i : array2){
             System.out.println("Here is new sctring with " + i);
+        }
+
+        System.out.println("\n \n");
+
+        ArrayList<String> langs1 = new ArrayList<String>();
+        langs1.add("Java");
+        langs1.add("Python");
+        langs1.add("C#");
+        langs1.add("JavaScript");
+
+        for (int i = 0; i < langs1.size(); i++){
+            System.out.println("ArrayList "  + langs1.get(i));
+        }
+
+        System.out.println("\n \n");
+
+        List<String> lang2 = Arrays.asList("Java", "Python", "C#", "JavaScript");
+
+        for(String language : lang2){
+            System.out.println("List here: " + language);
         }
     }
 }
